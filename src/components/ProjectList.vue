97aef8a5fb39066d99383e1c7f16fc4b6f2dd510
Quang LN <quangln2810@gmail.com>
2022-05-11 22:21:39 +0700

    fix style

--- src/components/ProjectList.vue
+++ src/components/ProjectList.vue
@@ -6,7 +6,7 @@
         <thead>
           <tr>
             <th>Workspace</th>
-            <th>Address</th>
+            <th style="width: 60%;">Address</th>
             <th></th>
           </tr>
         </thead>
@@ -15,10 +15,10 @@
             :class="{'selected': selectedProjectIdx === idx, 'shared': shareState(p) }"
             @click="$store.commit('selectPrjIndex', idx)">
             <td><i class="fa fa-sitemap fa-big-icon"></i><span style="margin-left:10px;">{{p.name}}</span></td>
-            <td style="width: 60%; font-style: italic; font-weight: 300">
+            <td>
               <input v-if='shareState(p)' type="text" :value="`\\\\hcm-dtpappserv.biendongpoc.vn\\codb_${normalizedUsername}_${p.name}`"
-                readonly style="width: 100%; border: 0; outline: 0; background: none;"
-                @click.stop="e => e.currentTarget.select()">
+                readonly style="width: 100%; border: 0; outline: 0; background: none; font-style: italic; font-weight: 300"
+                @click="e => e.currentTarget.select()">
             </td>
             <td style="width: 80px">
               <button @click="doUnshare(p)"
